209a95106b9aea1de9d257e40a94e49e2609cd58
yacine-benichou <78235417+yacine-benichou@users.noreply.github.com>
2022-04-25 19:57:08 +0200

    Projects page completed

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,25 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faCodepen, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  public isDarkThemeOn: boolean = true;
+  public themeStateObject: any;
+  public faGithub: IconDefinition = faGithub;
+  public faCodePen: IconDefinition = faCodepen;
+
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+    this.themeStateObject = this.location.getState();
+    this.isDarkThemeOn = this.themeStateObject.themeState;
   }
 
 }
